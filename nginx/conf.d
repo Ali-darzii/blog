28000293dc6fea84d471be4cf6b206a7ed9d5839
ali-darzii <ali.darzi.1354@gmail.com>
2025-03-09 15:32:57 +0330

    ngin

--- nginx/conf.d
+++ nginx/conf.d
@@ -2,28 +2,23 @@ upstream web_app {
     server backend:8000;
 }
 
-server {
-    server_name 192.168.56.104;
+events {}
 
-    access_log /var/log/nginx/access.log;
-    error_log /var/log/nginx/error.log;
+http {
 
-    location / {
-        proxy_pass http://web_app;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $host;
-        proxy_redirect off;
-    }
-}
+    server {
 
-server {
-    listen 80;
-    server_name 192.168.56.104;
+        listen80;
+        location / {
 
-    location ~/.well-known/acme-challenge {
-        allow all;
-        root /var/www/certbot;
+            proxy_pass http://192.168.56.104:8000;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP #remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_forwarded_for;
+
+        }
+
     }
 
-    return 301 https://$host:$request_uri;
+
 }
